a909509e610db8c0d21a5457ff74c8b02bdfe94b
Luciano Muñoz <luchisds@gmail.com>
2020-07-13 21:45:28 -0300

    Updates

---
+++ precache-manifest.faeece66c7778274ed86e6034a71e6df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbaad51a7196f0f20aed628ac750df57",
+    "url": "/nius/index.html"
+  },
+  {
+    "revision": "6eed270ff98b6fb3a3e2",
+    "url": "/nius/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5846983c1a4ec6d9e404",
+    "url": "/nius/static/js/2.52233b08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nius/static/js/2.52233b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eed270ff98b6fb3a3e2",
+    "url": "/nius/static/js/main.55547300.chunk.js"
+  },
+  {
+    "revision": "9d67864038f001cdbcad",
+    "url": "/nius/static/js/runtime-main.7104e874.js"
+  }
+]);
